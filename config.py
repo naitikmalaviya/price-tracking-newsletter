5191b74ae40690a7941fa4649aee4ff5c277ad01
Naitik <n.malaviya@innoloft.com>
2025-05-07 12:09:21 +0530

    Change logging level

--- config.py
+++ config.py
@@ -32,6 +32,6 @@ LOWEST_PRICE_DATE_PROPERTY_NAME = "Lowest Price Date"
 
 # Configure logging
 logging.basicConfig(
-    level=logging.WARNING,
+    level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
